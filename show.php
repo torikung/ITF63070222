b0fb4958b1e4c5c95b0426a77b4e5518a5a1e8d8
RaIDeN-201 <73694253+RaIDeN-201@users.noreply.github.com>
2020-11-14 11:37:26 +0700

    Update show.php

--- show.php
+++ show.php
@@ -9,38 +9,39 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<center>
 <?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'itfraiden.mysql.database.azure.com', 'raiden@itfraiden', 'itf_12644', 'itf63070222', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'itfraiden.mysql.database.azure.com', 'raiden@itfraiden', 'itf_12644', 'itf63070222', 3306);
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+    $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-<table class="table table-dark table-striped">
-    <thead>
+<div class="container">
+    <table width="600" border="1">
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                    <th width="100"> <div align="center">Name</div></th>
+                    <th width="350"> <div align="center">Comment </div></th>
+                    <th width="150"> <div align="center">Link </div></th>
+                </tr>
+            </thead>
+    <?php
+        while($Result = mysqli_fetch_array($res))
+        {
+    ?>
         <tr>
-            <th width="100"> <div align="center">Name</div></th>
-            <th width="350"> <div align="center">Comment </div></th>
-            <th width="150"> <div align="center">Link </div></th>
+            <td><?php echo $Result['Name'];?></div></td>
+            <td><?php echo $Result['Comment'];?></td>
+            <td><?php echo $Result['Link'];?></td>
         </tr>
-    </thead>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
+    <?php
+        }
+    ?>
+    </table>
+</div>
 <?php
 mysqli_close($conn);
 ?>
