76aa0cf2b535431a2f7100115b03cc055bc9c97d
RaIDeN-201 <73694253+RaIDeN-201@users.noreply.github.com>
2020-11-14 11:54:42 +0700

    Update show.php

--- show.php
+++ show.php
@@ -37,7 +37,8 @@
             <td><?php echo $Result['Name'];?></div></td>
             <td><?php echo $Result['Comment'];?></td>
             <td><?php echo $Result['Link'];?></td>
-            <td><button type="button" class="btn btn-light">แก้ไข</button><button type="button" class="btn btn-light">ลบ</button></td>
+            <td><button type="button" class="btn btn-light">แก้ไข</button>
+                <button type="button" class="btn btn-light">ลบ</button></td>
         </tr>
     <?php
         }
