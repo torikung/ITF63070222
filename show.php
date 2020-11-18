c4bf092e242e776e5983a356681d8d75b85d64e4
RaIDeN-201 <73694253+RaIDeN-201@users.noreply.github.com>
2020-11-18 22:27:26 +0700

    Update show.php

--- show.php
+++ show.php
@@ -27,8 +27,8 @@
                     <th width="100"> <div align="center">Name</div></th>
                     <th width="300"> <div align="center">Comment</div></th>
                     <th width="150"> <div align="center">Link</div></th>
-                    <th width="15%"> <div align="center">Edit</div></th>
-                    <th width="15%"> <div align="center">Delete</div></th>
+                    <th width="10%"> <div align="center">Edit</div></th>
+                    <th width="10%"> <div align="center">Delete</div></th>
                 </tr>
             </thead>
     <?php
