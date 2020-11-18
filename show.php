3461d2ddfaf2f9960f223a92a20fa7802fd017e6
RaIDeN-201 <73694253+RaIDeN-201@users.noreply.github.com>
2020-11-18 22:29:10 +0700

    Update show.php

--- show.php
+++ show.php
@@ -40,7 +40,7 @@
             <td><?php echo $Result['Comment'];?></td>
             <td><?php echo $Result['Link'];?></td>
             <td><div align="center">
-                    <form action="edit_form.php" method="post">
+                    <form action="edit.php" method="post">
                         <input type="hidden" name="ID" value=<?php echo $Result['ID'];?>>
                         <button type="button" class="btn btn-light">แก้ไข</button></form></div>
             </td>
