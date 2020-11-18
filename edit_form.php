4819021f86d2491e480962d63a9532aa2565ddd8
RaIDeN-201 <73694253+RaIDeN-201@users.noreply.github.com>
2020-11-19 00:44:12 +0700

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-	.form {
-  		align: center;
-	}
-</style>
     	<title>Edited Form</title>
 	<meta charset="utf-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
